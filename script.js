1d814b02fbc34b19ff03ed11da0527a28d0c6a39
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-08-29 23:06:46 -0600

    added begining of js file up to line 21

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//created generate password function
+function generatePassword (){
+    console.log("Button has been clicked.");
+
+    var pswdLength = window.prompt("What would you like your password length to be? Your password must be at least 8 charaters long, but no more than 128 characters long.");
+}
